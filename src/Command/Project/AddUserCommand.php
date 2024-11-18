f0b48f300fbad691a5c4872b08cd6059dad77657
Michal Blava <michal.blava@gmail.com>
2024-11-18 22:13:55 +0100

    tvorba admin usera

--- src/Command/Project/AddUserCommand.php
+++ src/Command/Project/AddUserCommand.php
@@ -77,11 +77,11 @@ class AddUserCommand extends Command
           'is_active' => 1,
         ])->id;
 
-//  $mUserRole = new \CeremonyCrmApp\Modules\Core\Settings\Models\UserRole($app);
-//  $idRoleAdministrator = $mUserRole->eloquent->create(['name' => 'Administrator'])->id;
+        $mUserRole = new \ADIOS\Models\UserRole($app);
+        $idRoleAdministrator = $mUserRole->eloquent->create(['name' => 'Administrator'])->id;
 
-//  $mUserHasRole = new \CeremonyCrmApp\Modules\Core\Settings\Models\UserHasRole($app);
-//  $mUserHasRole->eloquent->create(['id_user' => $idUserAdministrator, 'id_role' => $idRoleAdministrator])->id;
+        $mUserHasRole = new \ADIOS\Models\UserHasRole($app);
+        $mUserHasRole->eloquent->create(['id_user' => $idUserAdministrator, 'id_role' => $idRoleAdministrator])->id;
       } catch (\Exception $e) {
         echo $e->getMessage();
       }
